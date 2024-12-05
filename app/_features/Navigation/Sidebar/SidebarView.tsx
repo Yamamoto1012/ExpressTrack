d5518dde0d3908bcbd6b39889d991f0a8d7e6880
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-12-05 21:23:24 +0900

    feat: sidebarの作成

--- app/_features/Navigation/Sidebar/SidebarView.tsx
+++ app/_features/Navigation/Sidebar/SidebarView.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image"
+import Icon from "../../../../public/Twitter_profile.png"
 
 export const SidebarView = () => {
   return (
-    <>
-      Sidebar
-    </>
+    <div className=" h-screen w-[150px] bg-[#2A2A36]">
+      {/* アイコン */}
+      <div className="flex flex-col items-center h-full">
+        <div className="w-[120px] h-[120px]">
+          <Image src={Icon} alt="アイコン" width={120} height={120} className="rounded" />
+        </div>
+        <div className="mt-2">
+          <p>ユーザー名</p>
+        </div>
+      </div>
+      {/* メニュー */}
+      <div>
+        {/* メニューの内容 */}
+      </div>
+    </div>
   )
 }
